3a98db8e357c678bda496055512a2842641d200b
Alexander Rengkat <successinmotion11@gmail.com>
2022-03-20 22:21:29 +0100

    responsiveness

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CalculatorContextProvider from "./Components/CalculatorContext";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <CalculatorContextProvider>
         <Calculator />
       </CalculatorContextProvider>
